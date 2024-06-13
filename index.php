3f2f8384e8c6af2a0cfaf4a2bbcb4b70b59a52bd
Hassan-KreateStudio <128836688+Hassan-KreateStudio@users.noreply.github.com>
2024-06-13 09:10:07 +0300

    The index nav refactored

--- index.php
+++ index.php
@@ -11,31 +11,10 @@
 </head>
 <body>
     <img src="./images/background.jpg" alt="Background Image" class="background-img">
-    <nav class="navbar navbar-expand-lg navbar-dark ">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="navbar-nav mr-auto">
-                    <a class="nav-link" href="shop.html">Shop</a>
-                    <a class="nav-link" href="products.html">Products</a>
-                    <a class="nav-link" href="contact.html">Contact</a>
-                    <a class="nav-link" href="story.html">Our story</a>
-                </div>
-                <div class="navbar-nav">
-                    <a class="nav-link" href="signup.html">Signup</a>
-                    <a class="nav-link" href="login.html">Login</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-    
+    <?php include './templates/index_nav.php'; ?>
     <div class="header">
         <img src="./images/logo.png" alt="logo" style="max-width: 100px; height: auto;">
     </div>
-
     <!-- Add Bootstrap JavaScript -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
---
+++ templates/index_nav.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark ">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav mr-auto">
+                    <a class="nav-link" href="shop.php">Shop</a>
+                    <a class="nav-link" href="products.php">Products</a>
+                    <a class="nav-link" href="contact.php">Contact</a>
+                    <a class="nav-link" href="story.php">Our story</a>
+                </div>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="signup.php">Signup</a>
+                    <a class="nav-link" href="login.php">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>
